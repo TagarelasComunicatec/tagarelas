5207a26127c8495ae2e87954d27743aa3857e4fb
Ricardo Rodriguez <ricardo.domingos@gmail.com>
2016-11-16 10:45:32 -0200

    Correções de versão Spring

---
+++ src/main/java/br/edu/tagarelas/TagarelasApplication.java
@@ -0,0 +1,12 @@
+package br.edu.tagarelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagarelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagarelasApplication.class, args);
+	}
+}
